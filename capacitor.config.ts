586679a284f2fbb2a34cbafd9ee0c0d6550125fc
Cohen Erickson <cohenerickson@gmail.com>
2022-10-18 22:18:24 -0500

    Android Demo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.cohenerickson.timer',
-  appName: 'react-timer',
-  webDir: 'dist/assets',
-  bundledWebRuntime: false
+  appId: "com.cohenerickson.timer",
+  appName: "react-timer",
+  webDir: "dist/public",
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#000000FF"
+    }
+  }
 };
 
 export default config;
