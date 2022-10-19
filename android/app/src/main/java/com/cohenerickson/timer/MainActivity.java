a03d8340efd640f8be7eb9f3c5ff55bc6fb9a082
Cohen Erickson <cohenerickson@gmail.com>
2022-10-19 02:06:05 +0000

    init

---
+++ android/app/src/main/java/com/cohenerickson/timer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cohenerickson.timer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cohenerickson.timer',
+  appName: 'react-timer',
+  webDir: 'dist/assets',
+  bundledWebRuntime: false
+};
+
+export default config;
